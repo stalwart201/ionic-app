11faab6acd1393ba6b17d995f5c4d696e4e8e27d
stalwart <rishabh@patchus.in>
2018-03-20 19:36:15 +0530

    Updates lazy loading in homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,IonicPage } from 'ionic-angular';
+
+@IonicPage()
 
 @Component({
   selector: 'page-home',
@@ -8,7 +10,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+  		
   }
 
 }
